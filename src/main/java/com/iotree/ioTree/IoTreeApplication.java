a81e41f0041ded9ddb6d0828ea7f1c27cb18708d
Ouchen <ouchen_mohamed@outlook.com>
2022-04-18 10:14:43 +0000

    ioTree

---
+++ src/main/java/com/iotree/ioTree/IoTreeApplication.java
@@ -0,0 +1,13 @@
+package com.iotree.ioTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iotree/ioTree/IoTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotree.ioTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
