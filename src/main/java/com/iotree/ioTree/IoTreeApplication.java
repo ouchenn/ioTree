e2c56a552c7f4af634d49d7fbaf4c75fc10173ea
Ouchen <ouchen_mohamed@outlook.com>
2022-04-22 14:48:33 +0000

    bcrypt

--- src/main/java/com/iotree/ioTree/IoTreeApplication.java
+++ src/main/java/com/iotree/ioTree/IoTreeApplication.java
@@ -2,6 +2,8 @@ package com.iotree.ioTree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class IoTreeApplication {
@@ -10,4 +12,8 @@ public class IoTreeApplication {
 		SpringApplication.run(IoTreeApplication.class, args);
 	}
 
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/iotree/ioTree/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/iotree/ioTree/service/impl/AppUserServiceImpl.java
@@ -6,6 +6,7 @@ import com.iotree.ioTree.service.IAppUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,10 +16,15 @@ import java.util.List;
 public class AppUserServiceImpl implements IAppUserService {
 
     @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
     private AppUserRepository userRepo;
 
     @Override
     public AppUser createUser(AppUser user) {
+        String password = this.bCryptPasswordEncoder(user.getPassword());
+        user.setPassword(password);
         return userRepo.save(user);
     }
 
@@ -30,5 +36,9 @@ public class AppUserServiceImpl implements IAppUserService {
     @Override
     public AppUser getUser(String id) {
         return userRepo.findById(id).orElse(null);
+    }
+
+    public String bCryptPasswordEncoder(String password){
+        return bCryptPasswordEncoder.encode(password);
     }
 }
