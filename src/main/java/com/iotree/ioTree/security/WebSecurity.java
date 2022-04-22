f0f7b1d769440d27d03518228c7c028d752ed73f
Ouchen <ouchen_mohamed@outlook.com>
2022-04-22 15:05:46 +0000

    web security

---
+++ src/main/java/com/iotree/ioTree/security/WebSecurity.java
@@ -0,0 +1,23 @@
+package com.iotree.ioTree.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users")
+                .permitAll()
+                .anyRequest().authenticated();
+    }
+}
